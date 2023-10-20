92e41314ba8a04e54d24d4e11a430308f758498c
ivanova anastasija <ivanova.anastasiia@phystech.edu>
2023-10-20 16:37:44 +0300

    fix executor test

--- src/test/java/edu/hw2/task3/ExecutorTest.java
+++ src/test/java/edu/hw2/task3/ExecutorTest.java
@@ -1,37 +1,13 @@
 package edu.hw2.task3;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ExecutorTest {
-    private static final MockedStatic<Server> serverMock = Mockito.mockStatic(Server.class);
-    private static final StableConnection stableConnection = Mockito.mock(StableConnection.class);
-    private static final FaultyConnection faultyConnection = Mockito.mock(FaultyConnection.class);
-
-    @BeforeEach
-    public void setupTests() {//Если метод принимает String
-
-        Mockito.doNothing().when(stableConnection).close();
-        Mockito.doNothing().when(faultyConnection).close();
-//        serverMock.when(Server::execute).thenReturn(Void);
-
-
-//        Mockito.doNothing().when(serverMock).execute(Mockito.anyString()));
-//        Mockito.when(serverMock.execute(Mockito.anyString())).thenReturn(Void.TYPE.);
-//        when(Server.execute(String)).thenReturn();
-//        when(class2.validateObject(anyObj()).thenReturn(true);
-    }
-
     static Arguments[] zeroAttemptsExecutor() {
-        return new Arguments[]{
+        return new Arguments[] {
             Arguments.of(new PopularCommandExecutor(0)),
             Arguments.of(new PopularCommandExecutor(new DefaultConnectionManager(), 0)),
             Arguments.of(new PopularCommandExecutor(new FaultyConnectionManager(), 0))
@@ -39,23 +15,20 @@ public class ExecutorTest {
     }
 
     static Arguments[] oneAttemptExecutor() {
-        return new Arguments[]{
+        return new Arguments[] {
             Arguments.of(new PopularCommandExecutor(1)),
             Arguments.of(new PopularCommandExecutor(new DefaultConnectionManager(), 1)),
             Arguments.of(new PopularCommandExecutor(new FaultyConnectionManager(), 1))
         };
     }
 
-
     static Arguments[] thousandAttemptExecutor() {
-        return new Arguments[]{
+        return new Arguments[] {
             Arguments.of(new PopularCommandExecutor(1000)),
             Arguments.of(new PopularCommandExecutor(new DefaultConnectionManager(), 1000)),
             Arguments.of(new PopularCommandExecutor(new FaultyConnectionManager(), 1000))
         };
     }
-
-
 
     @ParameterizedTest
     @MethodSource("zeroAttemptsExecutor")
